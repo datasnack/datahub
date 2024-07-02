56bd5ba66ce6b502d89a9c569f7783b3fd05c998
Jonathan Ströbele <mail@jonathanstroebele.de>
2024-07-02 14:22:52 +0200

    Allow export of Shapes in CSV/Excel/Gpkg

--- shapes/management/commands/exportshapes.py
+++ shapes/management/commands/exportshapes.py
@@ -2,27 +2,48 @@ import argparse
 from pathlib import Path
 
 import geopandas
-from django.core.management.base import BaseCommand
+import pandas as pd
+from django.core.management.base import BaseCommand, CommandError
 from shapely import wkt
 
-from shapes.models import Shape, Type
+from shapes.models import Shape
 
 
 class Command(BaseCommand):
-    help = "Export loaded shapefiles"
+    help = "Export loaded Shapes in different formats"
 
     def add_arguments(self, parser):
-        parser.add_argument("--format", default=None)
         parser.add_argument(
-            "--file", nargs="?", default=None, help="Specify the filename for the dump"
+            "--file",
+            nargs="?",
+            default=None,
+            help="Specify the filename",
+        )
+
+        parser.add_argument(
+            "--format",
+            type=str,
+            choices=["csv", "xlsx", "gpkg"],
+            required=True,
+            help='Specify the format of the file. Must be one of "csv", "excel", or "gpkg".',
+        )
+
+        parser.add_argument(
+            "--geometry",
+            default=True,
+            action=argparse.BooleanOptionalAction,
+            help="Boolean flag to include geometry. Default is True.",
         )
 
     def handle(self, *args, **options):
-        format = options["format"]
-
         file = options["file"]
         if not file:
-            file = "shapes.csv"
+            file = f'shapes.{options["format"]}'
+
+        if not options["geometry"] and options["format"] == "gpkg":
+            raise CommandError(
+                "--no-geometry can't be used in conjunction with GPKG output."
+            )
 
         path = Path("data/") / file
         shapes = Shape.objects.all().order_by("type__position", "name")
@@ -39,11 +60,24 @@ class Command(BaseCommand):
             for k, v in s.properties.items():
                 r[k] = v
 
-            r["geometry"] = wkt.loads(s.geometry.wkt)
+            if options["geometry"]:
+                r["geometry"] = wkt.loads(s.geometry.wkt)
 
             rows.append(r)
 
-        gdf = geopandas.GeoDataFrame(rows, geometry="geometry")
-        gdf = gdf.set_crs(4326)
+        if options["geometry"]:
+            gdf = geopandas.GeoDataFrame(rows, geometry="geometry")
+            gdf = gdf.set_crs(4326)
+        else:
+            gdf = pd.DataFrame(rows)
 
-        gdf.to_csv(path, index=False)
+        # Save file according to format
+        match options["format"]:
+            case "csv":
+                gdf.to_csv(path, index=False)
+            case "xlsx":
+                gdf.to_excel(path, index=False)
+            case "gpkg":
+                gdf.to_file(path, index=False)
+            case _:
+                pass
