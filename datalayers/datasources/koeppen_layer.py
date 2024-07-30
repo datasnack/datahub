3f6b366f6407e5f62fadca286c0f8b8d1caea78d
Jonathan Ströbele <mail@jonathanstroebele.de>
2024-07-30 09:15:04 +0200

    feat: Update Koeppen Layer to latest publication

--- datalayers/datasources/koeppen_layer.py
+++ datalayers/datasources/koeppen_layer.py
@@ -1,5 +1,5 @@
-import os
 import subprocess
+from enum import Enum
 from pathlib import Path
 
 import numpy as np
@@ -11,37 +11,64 @@ from datalayers.datasources.tiff_layer import TiffLayer
 class KoeppenLayer(TiffLayer):
     """Extends TiffParameter class for Koeppen data consumption."""
 
-    def __init__(self):
+    class ClimateTypes(Enum):
+        Af = 1  # Tropical, rainforest
+        Am = 2  # Tropical, monsoon
+        Aw = 3  # Tropical, savannah
+        BWh = 4  # Arid, desert, hot
+        BWk = 5  # Arid, desert, cold
+        BSh = 6  # Arid, steppe, hot
+        BSk = 7  # Arid, steppe, cold
+        Csa = 8  # Temperate, dry summer, hot summer
+        Csb = 9  # Temperate, dry summer, warm summer
+        Csc = 10  # Temperate, dry summer, cold summer
+        Cwa = 11  # Temperate, dry winter, hot summer
+        Cwb = 12  # Temperate, dry winter, warm summer
+        Cwc = 13  # Temperate, dry winter, cold summer
+        Cfa = 14  # Temperate, no dry season, hot summer
+        Cfb = 15  # Temperate, no dry season, warm summer
+        Cfc = 16  # Temperate, no dry season, cold summer
+        Dsa = 17  # Cold, dry summer, hot summer
+        Dsb = 18  # Cold, dry summer, warm summer
+        Dsc = 19  # Cold, dry summer, cold summer
+        Dsd = 20  # Cold, dry summer, very cold winter
+        Dwa = 21  # Cold, dry winter, hot summer
+        Dwb = 22  # Cold, dry winter, warm summer
+        Dwc = 23  # Cold, dry winter, cold summer
+        Dwd = 24  # Cold, dry winter, very cold winter
+        Dfa = 25  # Cold, no dry season, hot summer
+        Dfb = 26  # Cold, no dry season, warm summer
+        Dfc = 27  # Cold, no dry season, cold summer
+        Dfd = 28  # Cold, no dry season, very cold winter
+        ET = 29  # Polar, tundra
+        EF = 30  # Polar, frost
+
+    def __init__(self) -> None:
         super().__init__()
 
         self.time_col = LayerTimeResolution.YEAR
         self.value_type = LayerValueType.PERCENTAGE
 
-        # GeoTiff files have no `NoData`-value set. But the value 0 is used
-        # for water bodies and has no meaning for the climate zones. We use
-        # this as NoData value.
-        self.manual_nodata = 0
-
-        self.area_of_interest = []
+        self.climate_types: list[self.ClimateTypes] = []
 
     def get_data_path(self) -> Path:
-        """Overwrite parameter_id based input directory, because we have
-        multiple derives parameters from this source."""
         return Path("./data/datalayers/koeppen/")
 
     def download(self):
-        url = "https://figshare.com/ndownloader/files/12407516"
+        url = "https://figshare.com/ndownloader/files/45057352"
+        file_name = "koppen_geiger_tif.zip"
 
-        if not os.path.isfile(self.get_data_path() / "Beck_KG_V1.zip"):
+        if not Path.is_file(self.get_data_path() / file_name):
             self._save_url_to_file(
-                url, folder=self.get_data_path(), file_name="Beck_KG_V1.zip"
+                url, folder=self.get_data_path(), file_name=file_name
             )
 
-        if os.path.isfile(self.get_data_path() / "legend.txt"):
+        if Path.is_file(self.get_data_path() / "legend.txt"):
             return
         try:
-            in_file = self.get_data_path() / "Beck_KG_V1.zip"
+            in_file = self.get_data_path() / file_name
             out_dir = self.get_data_path().as_posix()
+
             subprocess.run(
                 f"unzip {in_file} -d {out_dir}",
                 shell=True,
@@ -52,25 +79,35 @@ class KoeppenLayer(TiffLayer):
             self.layer.warning("Could not unzip files: %s", error.stderr)
 
     def get_tiff_files(self, param_dir):
-        # files = sorted([s for s in os.listdir(param_dir) if s.rpartition('.')[2] in ('tiff','tif')])
+        # We ignore 2041_2070 and 2071_2099 since we are not interested in predictions.
+        # Also we would need to decide on a specific scenario.
+        folders = ["1901_1930", "1931_1960", "1961_1990", "1991_2020"]
 
-        # only one file is of interest for us
-        return ["Beck_KG_V1_present_0p0083.tif"]
+        files = []
+        for folder in folders:
+            files.append(f"{folder}/koppen_geiger_0p00833333.tif")
+
+        return files
 
     def consume(self, file, band, shape):
         total_cells = np.count_nonzero(~np.isnan(band))
         values, count = np.unique(band, return_counts=True)
-        stats = dict(zip(values, count))
+        stats = dict(zip(values, count, strict=True))
 
         aoi_cells = 0
-        for key in self.area_of_interest:
-            if key in stats:
-                aoi_cells += stats[key]
+        for climate_type in self.climate_types:
+            if climate_type.value in stats:
+                aoi_cells += stats[climate_type.value]
+        proportion = aoi_cells / total_cells
 
-        self.rows.append(
-            {
-                "year": 2016,
-                "shape_id": shape.id,
-                "value": aoi_cells / total_cells,
-            }
-        )
+        year_start = int(file[0:4])
+        year_end = int(file[5:9])
+
+        for year in range(year_start, year_end + 1):
+            self.rows.append(
+                {
+                    "year": year,
+                    "shape_id": shape.id,
+                    "value": proportion,
+                }
+            )
