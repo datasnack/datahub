b629d4361b07aeb3b119650de2cc6412da53b088
Jonathan Ströbele <mail@jonathanstroebele.de>
2024-08-15 10:24:11 +0200

    fix: optimize layout of datalayer table

--- app/resources/js/components/Chart.svelte
+++ app/resources/js/components/Chart.svelte
@@ -131,71 +131,77 @@
 
 </script>
 
-<div>
-    <div bind:this={chart}></div>
+<div class="card bg-light mb-3">
 
-    <p class="mb-1 small">
-        Add data to the chart:
-    </p>
-    <div class="row mb-3">
-        <div class="col-4 col-md-2">
-            {#if datalayer.temporal_resolution == 'date' }
-            <select class="form-select" bind:value={resampleTo}>
-                <option value="">Original resolution</option>
-                <option value="W-MON">Resample on week (W-MON)</option>
-                <option value="M">Resample on month (M)</option>
-            </select>
-            {:else}
-                <div class="col-form-label text-muted fst-italic">No resampling.</div>
-            {/if}
-        </div>
-        <div class="col-8 col-md-4">
-            <select class="form-select" bind:value={shapeType} on:change={addShapeType}>
-                <option value="">Add shape types…</option>
-                {#each datalayer.shape_types as shape_type}
-                <option value="{ shape_type.key }">{ shape_type.name }</option>
-                {/each}
-            </select>
-        </div>
-
-        <div class="col-12 col-md-6">
-            <div bind:this={shapeselect}></div>
-        </div>
+    <div class="rounded-top overflow-hidden">
+        <div bind:this={chart}></div>
     </div>
 
+    <div class="card-body">
 
-    <p class="mb-1 small border-top pt-3">
-        Work with the chart:
-    </p>
-    <div class="row">
-        <div class="col-12 col-md-6">
 
-            <div class="input-group">
-                {#if datalayer.temporal_resolution == 'year' }
-                    <select class="form-select form-select-sm" bind:value={zoomStart}>
-                        <!-- expand the array with [...var] and reverse, so we don't reverse the original array! -->
-                        {#each [...datalayer.available_years].reverse() as year}
-                        <option value="{ year }">{ year }</option>
-                        {/each}
-                    </select>
-                    <select class="form-select form-select-sm" bind:value={zoomEnd}>
-                        {#each datalayer.available_years as year }
-                        <option value="{year}">{year}</option>
-                        {/each}
-                    </select>
-                {:else if datalayer.temporal_resolution == 'date'}
-                    <input type="date" class="form-control form-control-sm" bind:value={zoomStart} min="{ datalayer.first_time}" max="{ datalayer.last_time }">
-                    <input type="date" class="form-control form-control-sm" bind:value={zoomEnd} min="{ datalayer.first_time }" max="{ datalayer.last_time}">
+        <p class="mb-1">
+            Add data to the chart:
+        </p>
+        <div class="row mb-3">
+            <div class="col-4 col-md-2">
+                {#if datalayer.temporal_resolution == 'date' }
+                <select class="form-select" bind:value={resampleTo}>
+                    <option value="">Original resolution</option>
+                    <option value="W-MON">Resample on week (W-MON)</option>
+                    <option value="M">Resample on month (M)</option>
+                </select>
+                {:else}
+                    <div class="col-form-label text-muted fst-italic">No resampling.</div>
                 {/if}
-               <button on:click={zoomChart} class="btn btn-sm btn-outline-secondary" type="button">Zoom</button>
+            </div>
+            <div class="col-8 col-md-4">
+                <select class="form-select" bind:value={shapeType} on:change={addShapeType}>
+                    <option value="">Add shape types…</option>
+                    {#each datalayer.shape_types as shape_type}
+                    <option value="{ shape_type.key }">{ shape_type.name }</option>
+                    {/each}
+                </select>
             </div>
 
+            <div class="col-12 col-md-6">
+                <div bind:this={shapeselect}></div>
+            </div>
         </div>
-        <div class="col-12 col-md-6">
-            <div class="d-flex justify-content-end">
-                <button on:click={clearTraces} class="btn btn-sm btn-outline-secondary">Clear chart</button>
+
+
+        <p class="mb-1 border-top pt-3">
+            Work with the chart:
+        </p>
+        <div class="row">
+            <div class="col-12 col-md-6">
+
+                <div class="input-group">
+                    {#if datalayer.temporal_resolution == 'year' }
+                        <select class="form-select form-select-sm" bind:value={zoomStart}>
+                            <!-- expand the array with [...var] and reverse, so we don't reverse the original array! -->
+                            {#each [...datalayer.available_years].reverse() as year}
+                            <option value="{ year }">{ year }</option>
+                            {/each}
+                        </select>
+                        <select class="form-select form-select-sm" bind:value={zoomEnd}>
+                            {#each datalayer.available_years as year }
+                            <option value="{year}">{year}</option>
+                            {/each}
+                        </select>
+                    {:else if datalayer.temporal_resolution == 'date'}
+                        <input type="date" class="form-control form-control-sm" bind:value={zoomStart} min="{ datalayer.first_time}" max="{ datalayer.last_time }">
+                        <input type="date" class="form-control form-control-sm" bind:value={zoomEnd} min="{ datalayer.first_time }" max="{ datalayer.last_time}">
+                    {/if}
+                <button on:click={zoomChart} class="btn btn-sm btn-outline-secondary" type="button">Zoom</button>
+                </div>
+
+            </div>
+            <div class="col-12 col-md-6">
+                <div class="d-flex justify-content-end">
+                    <button on:click={clearTraces} class="btn btn-sm btn-outline-secondary">Clear chart</button>
+                </div>
             </div>
         </div>
     </div>
 </div>
-
