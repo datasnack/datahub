597fa578cc4f7b6a6215474a9562bd8ca9b298ac
michelle <michelleanastasya.valentina@haw-hamburg.de>
2024-10-15 20:57:15 +0200

    ????!!!!!?????????!!!!!!!!!!

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,6 +4,11 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path('slider/', views.slider_view, name='slider_view'),
-    path('slider/load-shapes/', views.load_shapes, name='load_shapes'),
+    path('temporal-trend/', views.temporal_trend_view, name='temporal_trend_view'),
+    path('temporal-trend/load-shapes/', views.load_shapes, name='load_shapes'),
+    path('temporal-trend/get-available-years/', views.get_available_years, name='get_available_years'),
+    path('temporal-trend/get-geometry-shape/', views.get_geometry_shape, name='get_geometry_shape'),
+    path('temporal-trend/get-datalayer-for-year/', views.get_datalayer_for_year, name='get_datalayer_for_year'),
+    path('temporal-trend/get-historical-data/', views.get_historical_data, name='get_historical_data'),
+    path('temporal-trend/get-datalayer-name/', views.get_datalayer_name, name='get_datalayer_name'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,9 @@
-from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render
+from collections import defaultdict
+
+from django.http import JsonResponse
+from django.shortcuts import render, get_object_or_404
 from django.conf import settings
+import datetime as dt
 
 from shapes.models import Type, Shape
 from datalayers.models import Datalayer
@@ -10,18 +13,18 @@ def home(request):
     return render(request, 'home.html')
 
 
-def slider_view(request):
-    types = Type.objects.all()
+def temporal_trend_view(request):
+    types = Type.objects.all().order_by('id')
     datalayers = Datalayer.objects.all()
 
     context = {
         'types': types,
         'datalayers': datalayers,
-        'datahub_center_x': settings.DATAHUB_CENTER_X,  # Pass center X
-        'datahub_center_y': settings.DATAHUB_CENTER_Y,  # Pass center Y
-        'datahub_center_zoom': settings.DATAHUB_CENTER_ZOOM  # Pass zoom level
+        'datahub_center_x': settings.DATAHUB_CENTER_X,
+        'datahub_center_y': settings.DATAHUB_CENTER_Y,
+        'datahub_center_zoom': settings.DATAHUB_CENTER_ZOOM
     }
-    return render(request, 'slider.html', context)
+    return render(request, 'temporal_trend.html', context)
 
 
 def load_shapes(request):
@@ -32,3 +35,76 @@ def load_shapes(request):
         shapes = Shape.objects.all()
     return JsonResponse(list(shapes.values('id', 'name')), safe=False)
 
+
+def get_available_years(request):
+    data_layer_key = request.GET.get('data_layer_key')
+    data_layer = Datalayer.objects.get(key=data_layer_key)
+    if data_layer:
+        available_years = data_layer.get_available_years
+    else:
+        available_years = []
+    return JsonResponse(available_years, safe=False)
+
+def get_geometry_shape(request):
+    shape_id = request.GET.get('shape_id')
+    shape = get_object_or_404(Shape, id=shape_id)
+
+    data = {
+        "type": "Feature",
+        "geometry": shape.geometry.geojson
+    }
+
+    return JsonResponse(data, safe=False)
+
+def get_datalayer_for_year(request):
+    data_layer_key = request.GET.get('data_layer_key')
+    shape_id = request.GET.get('shape_id')
+    year = int(request.GET.get('year'))
+
+    data_layer = get_object_or_404(Datalayer, key=data_layer_key)
+    shape = get_object_or_404(Shape, id=shape_id)
+
+    value_obj = data_layer.value(shape=shape, when=dt.datetime(year, 1, 1))
+
+    # # Calculate min and max values for the entire data layer across all shapes and years
+    # min_value =
+    # max_value =
+
+    if value_obj:
+        return JsonResponse({
+            'value': value_obj.value,
+            # 'min_value': min_value,
+            # 'max_value': max_value,
+        })
+    else:
+        return JsonResponse({'error': 'No data available'}, status=404)
+
+
+def get_historical_data(request):
+    shape_id = request.GET.get('shape_id')
+    data_layer_keys = request.GET.getlist('data_layer_keys[]')
+
+    shape = get_object_or_404(Shape, id=shape_id)
+
+    combined_data = defaultdict(list)
+
+    try:
+        for data_layer_key in data_layer_keys:
+            data_layer = get_object_or_404(Datalayer, key=data_layer_key)
+
+            years = data_layer.get_available_years
+
+            for year in years:
+                value_obj = data_layer.value(shape=shape, when=dt.datetime(year, 1, 1))
+                combined_data[year].append({
+                    'data_layer': data_layer_key,
+                    'value': value_obj.value
+                })
+
+        result = [{'year': year, 'values': values} for year, values in combined_data.items()]
+        result.sort(key=lambda x: x['year'])
+
+        return JsonResponse(result, safe=False)
+
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=400)
