7ab562273938ca94e5545549926037fd50f5d62e
Jonathan Ströbele <mail@jonathanstroebele.de>
2024-12-09 17:10:11 +0100

    fix: show datalayer popover with css/hover instead of Bootrap popover
    The Bootstrap Popover API didn't allow to show the Popup on hover/click *and* to also tio select/click links in the popover while also dissming the the popup with leaving/clicking somehwere else on the page.
    
    So for now it's just a simple popover with css hover/tap  state visibillity. The Browser Popover API should be evaluated further, but it was not immediatly clear how to accomplish the above described scenary directly with this api

--- app/resources/js/app.js
+++ app/resources/js/app.js
@@ -13,10 +13,6 @@ window.DataTable = DataTable;
 
 import * as bootstrap from 'bootstrap'
 
-const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
-const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
-
-
 /**
  * Leaflet
  *
