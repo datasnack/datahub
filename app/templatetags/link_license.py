e1f9611e9af79c9ebc2d03c4bd83edf3dc47fc66
Jonathan Ströbele <mail@jonathanstroebele.de>
2025-02-27 15:13:21 +0100

    link license with first available link from SPDX infos

--- app/templatetags/link_license.py
+++ app/templatetags/link_license.py
@@ -26,7 +26,7 @@ def link_license(license_id):
         license_info = SPDX[license_key]
 
         # in case of ONLY ONE further info link, we link the licenseId to this
-        if len(license_info["seeAlso"]) == 1:
+        if len(license_info["seeAlso"]) >= 1:
             return mark_safe(  # noqa: S308
                 f'<a title="{escape(license_info["name"])}" href="{escape(license_info["seeAlso"][0])}">{escape(license_id)}</a>'
             )
