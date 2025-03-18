f7183374880fefdd1998120891cdcdb77f10f49e
Jonathan Ströbele <mail@jonathanstroebele.de>
2025-03-18 18:57:57 +0100

    fix: import further scale since Legend depends on them in some cases

--- app/resources/js/d3/d3.js
+++ app/resources/js/d3/d3.js
@@ -2,6 +2,6 @@ export * from 'd3-array';
 export * from 'd3-axis';
 export * from 'd3-fetch';
 export * from 'd3-interpolate';
-export { scaleSequential } from 'd3-scale';
+export { scaleSequential, scaleThreshold, scaleLinear } from 'd3-scale';
 export * from 'd3-scale-chromatic';
 export * from 'd3-selection';
