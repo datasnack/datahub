129f8361b4f3b680d3a4fc09c2d10a12a2792134
Jonathan Ströbele <mail@jonathanstroebele.de>
2025-06-24 10:05:37 +0200

    pref: inline import for DataCiteRESTClient so prevent loading it vor unrleated task, like mgmt commands

--- datalayers/api/views_datacite.py
+++ datalayers/api/views_datacite.py
@@ -1,12 +1,12 @@
-from datacite import DataCiteRESTClient
-from datacite.errors import DataCiteNotFoundError
-
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 
 
 @login_required
 def datacite(request):
+    from datacite import DataCiteRESTClient
+    from datacite.errors import DataCiteNotFoundError
+
     dc = DataCiteRESTClient(None, None, None)
     res = {}
     try:
